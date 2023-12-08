f043702d93e5d07b5a1b493070668f57facb49c4
Pooja <pooja@qaoncloud.com>
2023-12-08 12:21:56 +0530

    First Commit

--- cypress/POM/Pom_scuti_wallet.cy.js
+++
@@ -1,9 +0,0 @@
-class ScutiWallet {
-  X_icon() {
-    return cy.get(".absolute > .h-6");
-  }
-  transaction_Icon() {
-    return cy.get("a[href='/transactions']");
-  }
-}
-export default new ScutiWallet();
